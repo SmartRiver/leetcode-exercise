2ebd6f0943d17dbfd967ed6f22357fec3ea44ae4
SmartRiver <johnsonhe0306@gmail.com>
2019-11-17 23:07:45 +0800

    modify

--- 392.判断子序列.py
+++ 392.判断子序列.py
@@ -47,9 +47,9 @@ class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
         for i in s:
             if i in t:
-                t = t[(t.index[i] + 1):]
+                t = t[t.index(i)+1:]
             else:
-                return False
+                return False    
         return True
 
 
